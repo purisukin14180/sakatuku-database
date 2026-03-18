49f8c7f577f12291964e8b4bf591eab6e506cc60
purisukin14180 <purisukin14180@gmail.com>
2026-03-18 17:56:42 +0900

    フォメコン追加

--- formations.js
+++ formations.js
@@ -1,4 +1,3 @@
-// formations.js
 const FORMATIONS_DATA = [
     // 初期解放(1コイン)
     { name: "4-4-2B ポゼッション", policy: "ポゼッション", formation: "4-4-2B", level: "初期", coin: 1, keys: "LDM(パサーⅠ以上)", effect: "ジャンプ・走力 30%UP" },
@@ -17,7 +16,7 @@ const FORMATIONS_DATA = [
     { name: "ロス・グアラニエス'02", policy: "ムービング", formation: "5-4-1 A", level: "初期", coin: 2, keys: "CB(組立CBⅠ以上)", effect: "キック力・マーク 50%UP" },
 
     // Lv.10解放
-    { name: "太極戦士'02", policy: "ポゼッション", formation: "3-4-3 B", level: "10", coin: 5, keys: "CB(ストッパーⅡ以上)、LM(ドリブラーⅡ以上)", effect: "キ精度・キープ力・ジャンプ 60%UP" },
+    { name: "太極戦士'02", policy: "ポゼッション", formation: "3-4-3 B", level: "10", coin: 5, keys: "CB(ストッパーⅡ以上)、LM(ドリブラーⅡ以上)", effect: "キック精度・キープ力・ジャンプ 60%UP" },
     { name: "ディアブル・ルージュ'18", policy: "ポゼッション", formation: "4-3-3 A", level: "10", coin: 5, keys: "RCB(組立CBⅡ以上)、RWG(ドリブラーⅡ以上)", effect: "キープ力・ジャンプ・スタミナ 60%UP" },
     { name: "ロス・プロス'01", policy: "カウンター", formation: "4-4-2 A", level: "10", coin: 5, keys: "GK(スイーパーGKⅡ以上)、RSB(守備的FBⅡ以上)", effect: "突破力・キープ力・ジャンプ 60%UP" },
     { name: "ラ・ブランキロハ'82", policy: "カウンター", formation: "4-4-2 E", level: "10", coin: 5, keys: "AM(アタッカーⅡ以上)、LAM(パサーⅡ以上)", effect: "突破力・セービング・コンタクト 60%UP" },
@@ -34,11 +33,17 @@ const FORMATIONS_DATA = [
 
     // Lv.20解放
     { name: "ラ・デア'22", policy: "ポゼッション", formation: "3-5-2 B", level: "20", coin: 20, keys: "CB(ストッパーⅡ以上)、RM(ドリブラーⅡ以上)、LCF(ラインブレーカーⅢ以上)", effect: "冷静さ・パスカット・ジャンプ・コンタクト 80%UP" },
-    { name: "ラ・ロハ'24", policy: "ポゼッション", formation: "4-3-3 B", level: "20", coin: 20, keys: "RB(攻撃的FBⅡ以上)、RDM(パサーⅡ以上)、RW(ドリブラーⅢ以上)", effect: "冷静さ・キ精度・パスカット・コンタクト 80%UP" },
-    { name: "レ・ブルー'98", policy: "カウンター", formation: "4-4-2 D", level: "20", coin: 20, keys: "LSB(守備的FBⅡ以上)、AM(セントラルMFⅡ以上)、RCF(ストライカーⅢ以上)", effect: "キ力・ジャンプ・コンタクト・敏捷性 80%UP" },
-    { name: "ディアボロ・ディ・ミラノ'99", policy: "カウンター", formation: "3-4-3 B", level: "20", coin: 20, keys: "RCB(組立CBⅡ以上)、RDM(セントラルMFⅢ以上)、LW(ドリブラーⅡ以上)", effect: "キ力・突破力・ジャンプ・コンタクト 80%UP" },
-    { name: "ゴーデンゾーネン'94", policy: "リアクション", formation: "3-4-3 C", level: "20", coin: 20, keys: "CB(ストッパーⅡ以上)、LDM(ハードマーカーⅡ以上)、RDM(セントラルMFⅢ以上)", effect: "キ精度・突破力・コンタクト・スタミナ 80%UP" },
+    { name: "ラ・ロハ'24", policy: "ポゼッション", formation: "4-3-3 B", level: "20", coin: 20, keys: "RB(攻撃的FBⅡ以上)、RDM(パサーⅡ以上)、RW(ドリブラーⅢ以上)", effect: "冷静さ・キック精度・パスカット・コンタクト 80%UP" },
+    { name: "レ・ブルー'98", policy: "カウンター", formation: "4-4-2 D", level: "20", coin: 20, keys: "LSB(守備的FBⅡ以上)、AM(セントラルMFⅡ以上)、RCF(ストライカーⅢ以上)", effect: "キック力・ジャンプ・コンタクト・敏捷性 80%UP" },
+    { name: "ディアボロ・ディ・ミラノ'99", policy: "カウンター", formation: "3-4-3 B", level: "20", coin: 20, keys: "RCB(組立CBⅡ以上)、RDM(セントラルMFⅢ以上)、LW(ドリブラーⅡ以上)", effect: "キープ力・突破力・ジャンプ・コンタクト 80%UP" },
+    { name: "ゴーデンゾーネン'94", policy: "リアクション", formation: "3-4-3 C", level: "20", coin: 20, keys: "CB(ストッパーⅡ以上)、LDM(ハードマーカーⅡ以上)、RDM(セントラルMFⅢ以上)", effect: "キック精度・突破力・コンタクト・スタミナ 80%UP" },
     { name: "セレソン・ダス・キナス'16", policy: "リアクション", formation: "4-4-2 A", level: "20", coin: 20, keys: "LSB(攻撃的FBⅡ以上)、LM(サイドアタッカーⅡ以上)、RCF(ストライカーⅢ以上)", effect: "パスカット・ジャンプ・コンタクト・走力 80%UP" },
-    { name: "ソベラーノ'94", policy: "ムービング", formation: "4-4-2 E", level: "20", coin: 20, keys: "LAM(パサーⅡ以上)、RAM(アタッカーⅡ以上)、LCF(ストライカーⅢ以上)", effect: "キ力・突破力・コンタクト・スタミナ 80%UP" },
-    { name: "アルビセレステ'01", policy: "ムービング", formation: "3-4-3 A", level: "20", coin: 20, keys: "LCB(組立CBⅡ以上)、LM(サイドアタッカーⅢ以上)、CF(ストライカーⅡ以上)", effect: "ジャンプ・コンタクト・走力・敏捷性 80%UP" }
+    { name: "ソベラーノ'94", policy: "ムービング", formation: "4-4-2 E", level: "20", coin: 20, keys: "LAM(パサーⅡ以上)、RAM(アタッカーⅡ以上)、LCF(ストライカーⅢ以上)", effect: "キープ力・突破力・コンタクト・スタミナ 80%UP" },
+    { name: "アルビセレステ'01", policy: "ムービング", formation: "3-4-3 A", level: "20", coin: 20, keys: "LCB(組立CBⅡ以上)、LM(サイドアタッカーⅢ以上)、CF(ストライカーⅡ以上)", effect: "ジャンプ・コンタクト・走力・敏捷性 80%UP" },
+
+    // イベント限定
+    { name: "イ・フリウラーニ'98", policy: "リアクション", formation: "3-4-3 B", level: "イベント", coin: 0, keys: "GK(オーソドックスGKⅡ以上)、LCB(組立CBⅡ以上)、LM(サイドアタッカーⅡ以上)", effect: "パスカット・キープ力・敏捷性 80%UP" },
+    { name: "シュヴァルツ・ゲルベン'16", policy: "カウンター", formation: "4-5-1 A", level: "イベント", coin: 0, keys: "GK(オーソドックスGKⅡ以上)、LCB(ストッパーⅡ以上)、RB(攻撃的FBⅡ以上)", effect: "コンタクト・走力・キック精度 80%UP" },
+    { name: "ダニッシュ・ダイナマイト'18", policy: "ムービング", formation: "4-5-1 B", level: "イベント", coin: 0, keys: "LCB(ストッパーⅡ以上)、AM(パサーⅡ以上)、CF(ストライカーⅡ以上)", effect: "コンタクト・走力・キープ力 80%UP" },
+    { name: "エル・レオン'70", policy: "ポゼッション", formation: "3-4-3 C", level: "イベント", coin: 0, keys: "CCB(ストッパーⅡ以上)、LAM(アタッカーⅡ以上)、LW(ドリブラーⅡ以上)", effect: "ロングパス・コンタクト・キープ力 80%UP" }
 ];
